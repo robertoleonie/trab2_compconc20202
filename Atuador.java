0a5a5d66359c79492cabb6abaab5a9a14a405d14
Thales Macena <thalesmacena@gmail.com>
2021-05-16 13:30:48 -0300

    Correção das regras de leitura de temperatura

--- Atuador.java
+++ Atuador.java
@@ -1,6 +1,6 @@
 public class Atuador extends Thread {
   int id;
-  FilaCircular<Elemento> fila;
+  FilaCircular fila;
   Elemento elemento;
   LE le;
   double media;
@@ -8,7 +8,7 @@ public class Atuador extends Thread {
   int contAlertaAmarelo;
   int contAlertaVermelho;
 
-  Atuador(int id, FilaCircular<Elemento> fila, LE le) {
+  Atuador(int id, FilaCircular fila, LE le) {
     this.id = id;
     this.fila = fila;
     this.le = le;
@@ -18,36 +18,55 @@ public class Atuador extends Thread {
   public void run() {
     while(true) {
       le.EntraLeitor(this.id);
-      contMedia = 0;
-      for (int i = fila.inicioDaFila; i <= fila.finalDaFila; i++) { // me incomda
-        if (this.id == fila.elementosDaFila[i].idSensor) {
-          this.media += this.fila.elementosDaFila[i].valor;
-          this.contMedia++;
-
-          if (fila.elementosDaFila[i].valor > 35) {
-           if (this.contAlertaAmarelo < 15) this.contAlertaAmarelo++;
-           this.contAlertaVermelho++;
-          } else {
-            if (this.contAlertaAmarelo > 0) this.contAlertaAmarelo--;
-            this.contAlertaVermelho = 0;
+      System.out.println("Atuador " + this.id + " esta lendo as temperaturas");
+      this.contMedia = 0;
+      this.media = 0;
+      this.contAlertaAmarelo = 0;
+      this.contAlertaVermelho = 0;
+      int i = fila.inicioDaFila - 1;
+      if (i >= -1) {
+        do {
+          i++;
+          if (i == fila.tamanhoMaximo) {
+            i = 0;
           }
+          if (this.id == fila.elementosDaFila[i].idSensor) {
+            this.media += this.fila.elementosDaFila[i].valor;
+            this.contMedia++;
 
-          if (this.contAlertaVermelho >= 5) {
-            System.out.println("Alerta Vermelho!");
-          } else if (this.contAlertaAmarelo >= 15) {
-            System.out.println("Alerta Amarelo!");
-          } else {
-            System.out.println("Condição normal");
+            if (fila.elementosDaFila[i].valor > 35) {
+              if (this.contAlertaAmarelo < 15)
+                this.contAlertaAmarelo++;
+              this.contAlertaVermelho++;
+            } else {
+              if (this.contAlertaAmarelo > 0)
+                this.contAlertaAmarelo--;
+              this.contAlertaVermelho = 0;
+            }
+
+            if (this.contAlertaVermelho >= 5) {
+              System.out.println("Atuador "+this.id+": ALERTA VERMELHO!!!");
+            } else if (this.contAlertaAmarelo >= 5) {
+              System.out.println("Atuador "+this.id+": Alerta Amarelo!");
+            } else {
+              System.out.println("Atuador "+this.id+": Condicao normal.");
+            }
           }
-        }
+        } while (i != fila.finalDaFila );
+      } else {
+        System.out.println("Atuador " + this.id + " nao leu nenhuma temperatura acima de 30 graus.");
       }
-
-      System.out.println("Media das leituras: " + this.media / this.contMedia);
+      if (this.contMedia == 0) {
+        System.out.println("Media das leituras do Atuador "+ this.id +": 0");
+      } else {
+        System.out.println(String.format("Temperatura media das leituras do Atuador %d: %.1f", this.id ,this.media/this.contMedia));
+      }
+      System.out.println("Atuador " + this.id + " terminou!");
       le.SaiLeitor(this.id);
       try {
         Thread.sleep(2000);
       } catch (InterruptedException e) {};
-      System.out.println("Atuador " + this.id + "terminou!");
+      
     }
   }
 }
