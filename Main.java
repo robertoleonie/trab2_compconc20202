387d9fab06df5cf2156cdfbc80125b5d11ef4352
Thales Macena <thalesmacena@gmail.com>
2021-05-16 13:35:11 -0300

    Remover generalização da FilaCircular, adicionar output para python e adicionar prioridade para as threads

--- Main.java
+++ Main.java
@@ -1,24 +1,32 @@
 public class Main {
   public static final int MAX_DIM = 60;
   public static void main (String[] args) {
-    if (args.length != 2) {
-      System.out.println(args[0] + " <num Leitores e Atuadores>");
+    if (args.length != 1) {
+      System.out.println("Digite: java Main <numSensores>");
       System.exit(-1);
     }
 
-    int nThreads = Integer.parseInt(args[1]);
+    int nThreads = Integer.parseInt(args[0]);
     Thread[] threads = new Thread[2*nThreads];
-    FilaCircular<Elemento> fila = new FilaCircular<>(MAX_DIM);
+    FilaCircular fila = new FilaCircular(MAX_DIM);
     LE le = new LE();
 
+    // Inicia o log de saida para Python
+    System.out.println ("import verificaLE");
+    System.out.println ("le = verificaLE.LE()");
+
     for (int i=0; i < nThreads; i++) {
-      threads[i] = new Sensor(i, fila, le);
-      threads[i].start();
+      threads[i] = new Sensor(i+1, fila, le);
+      threads[i].setPriority(Thread.NORM_PRIORITY); // Prioridade máxima para threads escritoras
     }
 
     for (int i=0; i < nThreads; i++) {
-      threads[i+nThreads] = new Atuador(i, fila, le);
-      threads[i+nThreads].start();
+      threads[i+nThreads] = new Atuador(i+1, fila, le);
+      threads[i+nThreads].setPriority(Thread.MAX_PRIORITY); // Prioridade menor para threads leitoras
+    }
+
+    for (int i=0; i < 2* nThreads; i++) {
+      threads[i].start();
     }
 
     for (int i=0; i< 2* nThreads; i++) {
